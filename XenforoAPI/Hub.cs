c87fdbe025d0765cf82c623e80ee50598e91f1ce
kolya112 <nikolayurch@gmail.com>
2023-06-04 22:55:42 +0300

    Добавьте файлы проекта.

---
+++ XenforoAPI/Hub.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace XenforoAPI
+{
+    public class Hub
+    {
+        protected string _apiKey;
+        protected string _baseURL;
+
+        public Hub(string url, string apiKey) 
+        {
+            _apiKey = apiKey;
+            _baseURL = url;
+        }
+
+        public partial class Users : XenforoAPI.Users
+        {
+            public Users(string url, string apiKey) : base(url, apiKey) { }
+        }
+
+        protected static async Task<string> GetRequestAsync(string url, string token, string user = "")
+        {
+            var header = new WebHeaderCollection
+            {
+                { "XF-Api-Key", token },
+                { "XF-Api-User", user }
+            };
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Accept = "application/json";
+            request.Headers = header;
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+
+        protected static async Task<string> PostRequestAsync(string url, string data, string token, string user = "", string contentType = "application/x-www-form-urlencoded", string method = "POST")
+        {
+            var headers = new WebHeaderCollection
+            {
+                { "XF-Api-Key", token },
+                { "XF-Api-User", user }
+            };
+
+            var request = (HttpWebRequest)WebRequest.Create(url + "/?" + data);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.ContentLength = data.Length;
+            request.ContentType = contentType;
+            request.Method = method;
+            request.Accept = "application/json";
+            request.Headers = headers;
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                await streamWriter.WriteAsync(data);
+            }
+            var response = (HttpWebResponse)await request.GetResponseAsync();
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                return await streamReader.ReadToEndAsync();
+            }
+        }
+    }
+}
---
+++ XenforoAPI/Users.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace XenforoAPI
+{
+    public class Users : Hub
+    {
+        public Users(string url, string apiKey) : base(url, apiKey) { }
+
+        public async Task<string> GetUsers(int page = 0)
+        {
+            return await GetRequestAsync($"{_baseURL}/api/users/?page={page}", _apiKey);
+        }
+
+        public async Task<string> CreateUserAsync(string data)
+        {
+            return await PostRequestAsync($"{_baseURL}/api/users/", data, _apiKey);
+        }
+
+        public async Task<string> FindUserByEmail(string email)
+        {
+            return await GetRequestAsync($"{_baseURL}/api/users/find-email?email={email}", _apiKey);
+        }
+
+        public async Task<string> FindUserByUsernamePrefix(string username)
+        {
+            return await GetRequestAsync($"{_baseURL}/api/users/find-name?username={username}", _apiKey);
+        }
+
+        public async Task<string> GetUserAsync(int userID, bool with_posts = false, int page = 0)
+        {
+            return await GetRequestAsync($"{_baseURL}/api/users/{userID}/?with_posts={with_posts.ToString().ToLower()}&page={page}", _apiKey);
+        }
+
+        public async Task<string> UpdateUserAsync(int userID, string data)
+        {
+            return await PostRequestAsync($"{_baseURL}/api/users/{userID}", data, _apiKey);
+        }
+
+        public async Task<string> DeleteUserAsync(int userID, string data = "")
+        {
+            return await PostRequestAsync($"{_baseURL}/api/users/{userID}/", data, _apiKey, method: "DELETE");
+        }
+
+        public async Task<string> DeleteUserAvatarAsync(int userID)
+        {
+            return await PostRequestAsync($"{_baseURL}/api/users/{userID}/avatar", "", _apiKey, method: "DELETE");
+        }
+
+        public async Task<string> GetUserProfilePostsAsync(int userID, int page = 0)
+        {
+            return await GetRequestAsync($"{_baseURL}/api/users/{userID}/profile-posts?page={page}", _apiKey);
+        }
+    }
+}
